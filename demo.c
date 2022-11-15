32ec8ec1ebeda530b33a8706bd68c3f1fbe8f0be
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-11-15 18:53:41 +0530

    second push

--- demo.c
+++ demo.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 void main()
 {
-    printf("this is demo....1");
+    printf("this is demo....1") ;
     printf("this is demo....2");
     printf("this is demo....3");
     printf("this is demo....4");
