eadbb277e20ca806792e4fcbac8fb98b3516d660
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-12-06 18:59:37 +0530

    array

---
+++ array5.c
@@ -0,0 +1,12 @@
+// Write a programe to accept name as an input from user
+#include<stdio.h>
+void main()
+{
+    char name[20];
+
+    printf("Enter your name ");
+    scanf("%s",&name);
+
+    printf("your name is %s ",name);
+
+}
---
+++ array6.c
@@ -0,0 +1,22 @@
+// Write a programe to accept name as an input from user and print it in reverse
+#include <stdio.h>
+void main()
+{
+    char name[25];
+    int count = 0,copy;
+    printf("Enter your name ");
+    for (count = 0; count < 25; count++)
+    {
+        scanf("%c", &name[count]);
+        if(name[count]=='\n')
+        {
+            break;
+        }
+    }
+    copy=count;
+    for(count=copy ; count>=0 ; count--)
+    {
+        printf("%c",name[count]);
+    }
+
+}
